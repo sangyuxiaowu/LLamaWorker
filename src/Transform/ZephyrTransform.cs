5aefd7d22bbd3f551b91b1e4e26c7319a1c21675
桑榆肖物 <sang93@qq.com>
2024-04-24 00:24:24 +0800

    feat: 添加测试phi3

---
+++ src/Transform/ZephyrTransform.cs
@@ -0,0 +1,35 @@
+﻿using LLama.Abstractions;
+using LLama.Common;
+using System.Text;
+
+namespace LLamaWorker.Transform
+{
+    /// <summary>
+    /// ChatML 历史记录转换
+    /// </summary>
+    public class ZephyrHistoryTransform : BaseHistoryTransform
+    {
+        /// <inheritdoc/>
+        protected override string userToken => "<|user|>";
+
+        /// <inheritdoc/>
+        protected override string assistantToken => "<|assistant|>";
+
+        /// <inheritdoc/>
+        protected override string systemToken => "<|system|>";
+
+        /// <inheritdoc/>
+        protected override string endToken => "<|end|>";
+    }
+
+    /// <summary>
+    /// 处理结尾多余的输出
+    /// </summary>
+    public class ZephyrTextStreamTransform
+        : BaseTextStreamTransform
+    {
+        /// <inheritdoc/>
+        protected override string startToken => "<|end|>";
+    }
+
+}
