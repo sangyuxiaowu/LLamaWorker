6d33d3d8b2caceffc28da84c2f803c80b2b4184c
桑榆肖物 <sang93@qq.com>
2024-05-07 23:15:11 +0800

    feat: 添加llama3

---
+++ src/Transform/LLamaTransform.cs
@@ -0,0 +1,35 @@
+﻿using LLama.Abstractions;
+using LLama.Common;
+using System.Text;
+
+namespace LLamaWorker.Transform
+{
+    /// <summary>
+    /// ChatML 历史记录转换
+    /// </summary>
+    public class LLamaHistoryTransform : BaseHistoryTransform
+    {
+        /// <inheritdoc/>
+        protected override string userToken => "<|start_header_id|>user<|end_header_id|>\n";
+
+        /// <inheritdoc/>
+        protected override string assistantToken => "<|start_header_id|>assistant<|end_header_id|>\n";
+
+        /// <inheritdoc/>
+        protected override string systemToken => "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n";
+
+        /// <inheritdoc/>
+        protected override string endToken => "<|eot_id|>";
+    }
+
+    /// <summary>
+    /// 处理结尾多余的输出
+    /// </summary>
+    public class LLamaTextStreamTransform
+        : BaseTextStreamTransform
+    {
+        /// <inheritdoc/>
+        protected override string startToken => "<|eot_id|>";
+    }
+
+}
