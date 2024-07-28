3ca926cb5ffc212d2aee25b264371f439a14f901
桑榆肖物 <sang93@qq.com>
2024-07-28 13:24:44 +0800

    test: 添加Semantic Kernel测试案例 - 简单测试 - 工具调用测试

---
+++ test/Simple/Program.cs
@@ -0,0 +1,18 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddLogging(c => c.SetMinimumLevel(LogLevel.Trace).AddConsole());
+
+builder.AddOpenAIChatCompletion("default", new OpenAIClient(new Uri("http://127.0.0.1:5000"), new Azure.AzureKeyCredential("key")));
+
+var kernel = builder.Build();
+
+
+IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+var result = await chatCompletionService.GetChatMessageContentsAsync("hello world");
+Console.WriteLine(result.FirstOrDefault()?.Content);
